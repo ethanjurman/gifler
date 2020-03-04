411a7c3c092b7720bfad238b35479738857b375d
Ethan Jurman <ethanjurman@gmail.com>
2020-03-04 17:06:30 -0500

    floor values

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const os = require("os");
-const { app, BrowserWindow, screen, ipcMain } = require("electron");
-const { execSync, exec } = require("child_process");
-const ffmpeg = require("ffmpeg-static-electron");
+const os = require('os');
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
+const { execSync, exec } = require('child_process');
+const ffmpeg = require('ffmpeg-static-electron');
 
 let drawWindow;
 let dpiScale = 1;
@@ -13,13 +13,13 @@ const makeTransparentWindow = () => {
     width: screenWindow.width,
     height: screenWindow.height,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
     transparent: true,
     frame: false,
-    show: false
+    show: false,
   });
-  transparentWindow.loadFile("./transparent.html");
+  transparentWindow.loadFile('./transparent.html');
   // transparentWindow.setAlwaysOnTop(true);
   transparentWindow.setResizable(false);
   // transparentWindow.setIgnoreMouseEvents(true);
@@ -34,12 +34,12 @@ const createWindow = () => {
     height: 600,
     show: false,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   // and load the index.html file
-  window.loadFile("./index.html");
+  window.loadFile('./index.html');
 
   // Open DevTools
   // window.webContents.openDevTools();
@@ -55,7 +55,7 @@ app
     console.error(err);
   });
 
-ipcMain.on("message", (event, arg) => {
+ipcMain.on('message', (event, arg) => {
   console.log({ arg });
 });
 
@@ -68,65 +68,63 @@ const logError = (error, stdout, stderr) => {
 };
 
 const stopRecording = () => {
-  console.log("STOP RECORDING");
-  ffmpegProcess.stdin.write("q");
+  console.log('STOP RECORDING');
+  ffmpegProcess.stdin.write('q');
 
   const FPS = 24;
   const SCALE = 1080;
-  const filepath = "output.mkv";
+  const filepath = 'output.mkv';
 
   // create palette
   const palatte = execSync(
     `${ffmpeg.path} -y -i ${filepath} -vf "fps=${FPS}, scale=${SCALE}:-1:flags=lanczos,palettegen" palette.png`,
-    logError
+    logError,
   );
   const gif = execSync(
     `${ffmpeg.path} -y -i ${filepath} -i palette.png -q 0 -filter_complex "fps=${FPS},scale=${SCALE}:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle" ${filepath}.gif`,
-    logError
+    logError,
   );
 
   app.quit();
 };
 
 ipcMain.on(
-  "startRecording",
+  'startRecording',
   (__event, { x, y, width, height, clientScreenDifX, clientScreenDifY }) => {
-    console.log("START RECORDING", {
+    console.log('START RECORDING', {
       x,
       y,
       width,
       height,
       clientScreenDifX,
-      clientScreenDifY
+      clientScreenDifY,
     });
     drawWindow.minimize();
-    const outputFile = "output.mkv";
+    const outputFile = 'output.mkv';
 
-    if (os.platform() === "darwin") {
+    const xValue = Math.floor((x - clientScreenDifX) * dpiScale);
+    const yValue = Math.floor((y - clientScreenDifY) * dpiScale);
+    const heightValue = Math.floor(height * dpiScale);
+    const widthValue = Math.floor(width * dpiScale);
+
+    if (os.platform() === 'darwin') {
       // on mac
       ffmpegProcess = exec(
-        `${
-          ffmpeg.path
-        } -y -f avfoundation -pix_fmt yuyv422 -framerate 24 -capture_cursor 1 -capture_mouse_clicks 1 -i "1:none" -vf "crop=${width *
-          dpiScale}:${height * dpiScale}:${(x - clientScreenDifX) *
-          dpiScale}:${(y - clientScreenDifY) * dpiScale}" ${outputFile}`,
-        logError
+        `${ffmpeg.path} -y -f avfoundation -pix_fmt yuyv422 -framerate 24 -capture_cursor 1 -capture_mouse_clicks 1 -i "1:none" -vf "crop=${widthValue}:${heightValue}:${xValue}:${yValue}" ${outputFile}`,
+        logError,
       );
     }
-    if (os.platform() === "win32") {
+    if (os.platform() === 'win32') {
       // on pc
       ffmpegProcess = exec(
-        `${ffmpeg.path} -y -f gdigrab -framerate 30 -offset_x ${x *
-          dpiScale} -offset_y ${y * dpiScale} -video_size ${width *
-          dpiScale}x${height *
-          dpiScale} -show_region 1 -i desktop ${outputFile}`,
-        logError
+        `${ffmpeg.path} -y -f gdigrab -framerate 30 -offset_x ${xValue} -offset_y ${yValue} -video_size ${widthValue}x${heightValue} -i desktop ${outputFile}`,
+        logError,
       );
     }
     // TODO SUPPORT OTHER SYSTEMS
 
-    drawWindow.on("focus", stopRecording);
-  }
+    drawWindow.on('focus', stopRecording);
+  },
 );
 
-ipcMain.on("stopRecording", stopRecording);
+ipcMain.on('stopRecording', stopRecording);
