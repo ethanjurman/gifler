6a6e7ce23dc220cde8ae68666e2a371ff5787fb5
Ethan Jurman <ejurman@hubspot.com>
2020-03-04 08:12:38 -0500

    system and dpi support

--- drawBox.js
+++ drawBox.js
@@ -1,13 +1,16 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
 // console.log(ipcRenderer.sendSync('sync-message', 'ping'));
 
-const RECORDING = 'RECORDING';
-const NOT_RECORDING = 'NOT_RECORDING';
+const RECORDING = "RECORDING";
+const NOT_RECORDING = "NOT_RECORDING";
 
 const boxCoor = [];
 let index = 0;
 let state = NOT_RECORDING;
+
+let clientScreenDifX = 0;
+let clientScreenDifY = 0;
 
 const drawBox = e => {
   if (state === RECORDING) {
@@ -17,7 +20,10 @@ const drawBox = e => {
   const x = e.clientX;
   const y = e.clientY;
 
-  ipcRenderer.send('message', { x, y });
+  clientScreenDifX = e.clientX - e.screenX;
+  clientScreenDifY = e.clientY - e.screenY;
+
+  ipcRenderer.send("message", { x, y });
 
   boxCoor[index] = { x, y };
   index = index === 0 ? 1 : 0;
@@ -30,41 +36,41 @@ const drawBox = e => {
     const right = Math.max(boxCoor[0].x, boxCoor[1].x);
     const top = Math.min(boxCoor[0].y, boxCoor[1].y);
     const bottom = Math.max(boxCoor[0].y, boxCoor[1].y);
-    const box = document.getElementById('whiteBox');
+    const box = document.getElementById("whiteBox");
     box.setAttribute(
-      'style',
+      "style",
       `
         width:${width}px;
         height:${height}px;
         left:${left}px;
         top:${top}px;
-    `,
+    `
     );
     console.log({ top, left, right, bottom });
-    const boxTop = document.getElementById('darkBoxTop');
-    boxTop.setAttribute('style', `bottom: ${window.innerHeight - top}px`);
-    const boxLeft = document.getElementById('darkBoxLeft');
+    const boxTop = document.getElementById("darkBoxTop");
+    boxTop.setAttribute("style", `bottom: ${window.innerHeight - top}px`);
+    const boxLeft = document.getElementById("darkBoxLeft");
     boxLeft.setAttribute(
-      'style',
+      "style",
       `right: ${window.innerWidth -
-        left}px; top: ${top}px; bottom: ${window.innerHeight - bottom}px`,
+        left}px; top: ${top}px; bottom: ${window.innerHeight - bottom}px`
     );
-    const boxRight = document.getElementById('darkBoxRight');
+    const boxRight = document.getElementById("darkBoxRight");
     boxRight.setAttribute(
-      'style',
+      "style",
       `left: ${right}px; top: ${top}px; bottom: ${window.innerHeight -
-        bottom}px`,
+        bottom}px`
     );
-    const boxBottom = document.getElementById('darkBoxBottom');
-    boxBottom.setAttribute('style', `top: ${bottom}px`);
+    const boxBottom = document.getElementById("darkBoxBottom");
+    boxBottom.setAttribute("style", `top: ${bottom}px`);
   }
 };
 
 const handleKeyPress = evt => {
-  if (evt.code === 'Enter') {
+  if (evt.code === "Enter") {
     startRecording();
   }
-  if (evt.code === 'Escape') {
+  if (evt.code === "Escape") {
     stopRecording();
   }
 };
@@ -75,16 +81,28 @@ const startRecording = () => {
   const x = Math.min(boxCoor[0].x, boxCoor[1].x);
   const y = Math.min(boxCoor[0].y, boxCoor[1].y);
 
-  ipcRenderer.send('startRecording', { width, height, x, y });
+  ipcRenderer.send("startRecording", {
+    width,
+    height,
+    x,
+    y,
+    clientScreenDifX,
+    clientScreenDifY
+  });
+
+  const boxes = [...document.getElementsByClassName("darkBox")];
+  boxes.forEach(box => {
+    box.setAttribute("style", "background: rgba(0,0,0,0)");
+  });
   state = RECORDING;
 };
 
 const stopRecording = () => {
-  ipcRenderer.send('stopRecording');
+  ipcRenderer.send("stopRecording");
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const clickArea = document.getElementById('clickArea');
-  clickArea.addEventListener('click', drawBox);
-  clickArea.addEventListener('keydown', handleKeyPress);
+document.addEventListener("DOMContentLoaded", () => {
+  const clickArea = document.getElementById("clickArea");
+  clickArea.addEventListener("click", drawBox);
+  clickArea.addEventListener("keydown", handleKeyPress);
 });
--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-const { app, BrowserWindow, screen, ipcMain } = require('electron');
-const { exec } = require('child_process');
-const ffmpeg = require('ffmpeg-static-electron');
+const os = require("os");
+const { app, BrowserWindow, screen, ipcMain } = require("electron");
+const { execSync, exec } = require("child_process");
+const ffmpeg = require("ffmpeg-static-electron");
 
 let drawWindow;
+let dpiScale = 1;
 
 const makeTransparentWindow = () => {
   const screenWindow = screen.getPrimaryDisplay().workAreaSize;
+  dpiScale = screen.getPrimaryDisplay().scaleFactor;
   const transparentWindow = new BrowserWindow({
     width: screenWindow.width,
     height: screenWindow.height,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true
     },
     transparent: true,
     frame: false,
-    show: false,
+    show: false
   });
-  transparentWindow.loadFile('./transparent.html');
+  transparentWindow.loadFile("./transparent.html");
   // transparentWindow.setAlwaysOnTop(true);
   transparentWindow.setResizable(false);
   // transparentWindow.setIgnoreMouseEvents(true);
@@ -31,12 +34,12 @@ const createWindow = () => {
     height: 600,
     show: false,
     webPreferences: {
-      nodeIntegration: true,
-    },
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html file
-  window.loadFile('./index.html');
+  window.loadFile("./index.html");
 
   // Open DevTools
   // window.webContents.openDevTools();
@@ -52,33 +55,78 @@ app
     console.error(err);
   });
 
-ipcMain.on('message', (event, arg) => {
+ipcMain.on("message", (event, arg) => {
   console.log({ arg });
 });
 
 let ffmpegProcess;
 
+const logError = (error, stdout, stderr) => {
+  if (error) {
+    console.error({ error, stderr });
+  }
+};
+
 const stopRecording = () => {
-  console.log('STOP RECORDING');
-  ffmpegProcess.stdin.write('q');
+  console.log("STOP RECORDING");
+  ffmpegProcess.stdin.write("q");
+
+  const FPS = 24;
+  const SCALE = 1080;
+  const filepath = "output.mkv";
+
+  // create palette
+  const palatte = execSync(
+    `${ffmpeg.path} -y -i ${filepath} -vf "fps=${FPS}, scale=${SCALE}:-1:flags=lanczos,palettegen" palette.png`,
+    logError
+  );
+  const gif = execSync(
+    `${ffmpeg.path} -y -i ${filepath} -i palette.png -q 0 -filter_complex "fps=${FPS},scale=${SCALE}:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle" ${filepath}.gif`,
+    logError
+  );
+
   app.quit();
 };
 
-ipcMain.on('startRecording', (__event, { x, y, width, height }) => {
-  console.log('START RECORDING');
-  drawWindow.minimize();
-  const outputFile = 'output.gif';
+ipcMain.on(
+  "startRecording",
+  (__event, { x, y, width, height, clientScreenDifX, clientScreenDifY }) => {
+    console.log("START RECORDING", {
+      x,
+      y,
+      width,
+      height,
+      clientScreenDifX,
+      clientScreenDifY
+    });
+    drawWindow.minimize();
+    const outputFile = "output.mkv";
 
-  ffmpegProcess = exec(
-    `${ffmpeg.path} -y -f gdigrab -framerate 30 -offset_x ${x} -offset_y ${y} -video_size ${width}x${height} -show_region 1 -i desktop ${outputFile}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error({ error, stderr });
-      }
-    },
-  );
+    if (os.platform() === "darwin") {
+      // on mac
+      ffmpegProcess = exec(
+        `${
+          ffmpeg.path
+        } -y -f avfoundation -pix_fmt yuyv422 -framerate 24 -capture_cursor 1 -capture_mouse_clicks 1 -i "1:none" -vf "crop=${width *
+          dpiScale}:${height * dpiScale}:${(x - clientScreenDifX) *
+          dpiScale}:${(y - clientScreenDifY) * dpiScale}" ${outputFile}`,
+        logError
+      );
+    }
+    if (os.platform() === "win32") {
+      // on pc
+      ffmpegProcess = exec(
+        `${ffmpeg.path} -y -f gdigrab -framerate 30 -offset_x ${x *
+          dpiScale} -offset_y ${y * dpiScale} -video_size ${width *
+          dpiScale}x${height *
+          dpiScale} -show_region 1 -i desktop ${outputFile}`,
+        logError
+      );
+    }
+    // TODO SUPPORT OTHER SYSTEMS
 
-  drawWindow.on('focus', stopRecording);
-});
+    drawWindow.on("focus", stopRecording);
+  }
+);
 
-ipcMain.on('stopRecording', stopRecording);
+ipcMain.on("stopRecording", stopRecording);
